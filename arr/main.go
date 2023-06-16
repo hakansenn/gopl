675a3d3e40be4e242e39fca1d5920fce662f716b
Hakan SEN <115928237+hakansenn@users.noreply.github.com>
2023-06-16 11:24:11 +0000

    deneme

--- arr/main.go
+++ arr/main.go
@@ -13,6 +13,18 @@ const(
 	FlagMulticast
 )
 
+func IsUp(v flags) bool { return v&FlagUp == FlagUp }
+func TurnDown(v *flags) { *v &^= FlagUp }
+func SetBroadcast(v *flags) { *v |= FlagBroadcast }
+func IsCast(v flags) bool { return v&(FlagBroadcast|FlagMulticast) != 0 }
+
+
 func main() {
-	fmt.Println(FlagUp, FlagBroadcast, FlagLoopback, FlagPointToPoint, FlagMulticast)
+	var v flags = FlagMulticast | FlagUp
+	fmt.Printf("%b %t\n", v, IsUp(v)) // "10001 true"
+	TurnDown(&v)
+	fmt.Printf("%b %t\n", v, IsUp(v)) // "10000 false"
+	SetBroadcast(&v)
+	fmt.Printf("%b %t\n", v, IsUp(v)) // "10010 false"
+	fmt.Printf("%b %t\n", v, IsCast(v)) // "10010 true"
 }
